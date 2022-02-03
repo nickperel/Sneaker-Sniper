514c15c93551e885210e8f1ae6ee1a9e5d1bdfcb
Kakashi <nickperel@live.com>
2022-02-02 22:09:26 -0500

    deleted unncecessary seeds.sql file, removed test text from README, adjusted Types model

---
+++ models/Types.js
@@ -0,0 +1,34 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Types extends Model {}
+
+Types.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+    brand_id: {
+      type: DataTypes.INTEGER,
+      references: {
+          model: 'brands',
+          key: 'id'
+      }
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'types',
+  }
+);
+
+module.exports = Types;
